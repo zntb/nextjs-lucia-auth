135b9e510700d151a3e529472c02a91f68120d6b
zntb <zenetibi@yahoo.com>
2024-05-05 12:39:51 +0300

    Create sign-up page

---
+++ app/(pages)/sign-up/page.tsx
@@ -0,0 +1,27 @@
+import SignUpForm from '@/components/SignUpForm';
+import Image from 'next/image';
+
+export default async function SignUpPage() {
+  return (
+    <div className="pt:mt-0 mx-auto flex flex-col items-center justify-center px-6 pt-8 dark:bg-gray-900 md:h-screen">
+      <a
+        href="#"
+        className="mb-8 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-10"
+      >
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          width={100}
+          height={24}
+          className="mr-4 h-11"
+        />
+      </a>
+      <div className="w-full max-w-xl space-y-8 rounded-lg bg-white p-6 shadow dark:bg-gray-800 sm:p-8">
+        <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+          Create a Free Account
+        </h2>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
---
+++ components/SignUpForm.tsx
@@ -0,0 +1,7 @@
+export default function SignUpForm() {
+  return (
+    <div>
+      <h1>Signup</h1>
+    </div>
+  );
+}
